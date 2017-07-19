7151b40959d90d7123d3116fe840a7a022df36a0
binLearning <binlearning@163.com>
2017-07-19 22:38:21 +0800

    Update l2normalization_layer.hpp

--- include/caffe/layers/l2normalization_layer.hpp
+++ include/caffe/layers/l2normalization_layer.hpp
@@ -10,29 +10,29 @@
 namespace caffe {
 
 template <typename Dtype>
-	class L2NormalizationLayer : public Layer<Dtype> {
-	public:
-		explicit L2NormalizationLayer(const LayerParameter& param)
-			  : Layer<Dtype>(param) {}
-		virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-			  const vector<Blob<Dtype>*>& top);
+class L2NormalizationLayer : public Layer<Dtype> {
+ public:
+  explicit L2NormalizationLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
 
-		virtual inline const char* type() const { return "L2Normalization"; }
-		virtual inline int ExactNumBottomBlobs() const { return 1; }
-		virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline const char* type() const { return "L2Normalization"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
 
-	protected:
-		virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-			  const vector<Blob<Dtype>*>& top);
-		virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-			  const vector<Blob<Dtype>*>& top);
-		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-			  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-		virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-			  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-		Blob<Dtype> squared_;
-	};
+Blob<Dtype> squared_;
+};
 
 }  // namespace caffe
 
